b5b90a8f2595ed67ff6dcac31137b6188749f095
HenryM8 <henryrm8@gmail.com>
2024-06-25 11:42:27 -0600

    Model for user and restaurant

---
+++ CGWEB/CGWEB/Models/RestaurantModel.cs
@@ -0,0 +1,133 @@
+﻿using CGWEB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace CGWEB.Models
+{
+    public class RestaurantModel
+    {
+        string urlWebApi = ConfigurationManager.AppSettings["urlWebApi"].ToString();
+
+        public long AddRestaurant(RestaurantEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/AddRestaurant";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<long>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<RestaurantEnt> GetRestaurants()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetRestaurants";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<RestaurantEnt>>().Result;
+                }
+
+                return new List<RestaurantEnt>();
+            }
+        }
+
+        public RestaurantEnt GetRestaurant(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetRestaurant?q=" + q;
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<RestaurantEnt>().Result;
+                }
+
+                return new RestaurantEnt();
+            }
+        }
+
+        public int ChangeRestaurantVisibility(RestaurantEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ChangeRestaurantVisibility";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public int ValidateRestaurantData(RestaurantEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ValidateRestaurantData";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public void UpdateRestaurantImage(RestaurantEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/UpdateRestaurantImage";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+                JsonContent body = JsonContent.Create(entidad);
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+            }
+        }
+    }
+}
---
+++ CGWEB/CGWEB/Models/UserModel.cs
@@ -0,0 +1,163 @@
+﻿using CGWEB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace CGWEB.Models
+{
+    public class UserModel
+    {
+        string urlWebApi = ConfigurationManager.AppSettings["urlWebApi"].ToString();
+
+        public int AddUser(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/AddUser";
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<UserEnt> GetUsers()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetUsers";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<UserEnt>>().Result;
+                }
+
+                return new List<UserEnt>();
+            }
+        }
+
+        public UserEnt GetUser(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetUser?q=" + q;
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<UserEnt>().Result;
+                }
+
+                return null;
+            }
+        }
+
+        public UserEnt Login(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/Login";
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<UserEnt>().Result;
+                }
+
+                return null;
+            }
+        }
+
+        public int ValidateUserData(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ValidateUserData";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public bool RecoveryPassword(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/RecoveryPassword";
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<bool>().Result;
+                }
+
+                return false;
+            }
+        }
+
+        public int ChangePassword(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ChangePassword";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public void UpdateUserImage(UserEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/UpdateUserImage";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+                JsonContent body = JsonContent.Create(entidad);
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+            }
+        }
+    }
+}
