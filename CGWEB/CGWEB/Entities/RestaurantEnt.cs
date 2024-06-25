7ecf2790dd270654fcee341582d1c169b5436234
HenryM8 <henryrm8@gmail.com>
2024-06-25 11:40:42 -0600

    Entities for user and restaurant

---
+++ CGWEB/CGWEB/Entities/RestaurantEnt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGWEB.Entities
+{
+    public class RestaurantEnt
+    {
+        public string Restaurant_name { get; set; }
+        public string Address { get; set; }
+        public string Phone_number { get; set; }
+        public string Website { get; set; }
+        public string Url_image { get; set; }
+        public bool Is_visible { get; set; }
+        public string Description { get; set; }
+        public string Location_map { get; set; }
+        public long Restaurant_id { get; set; }
+        public long Reviews_count { get; set; }
+        public decimal Stars_count { get; set; }
+    }
+}
---
+++ CGWEB/CGWEB/Entities/UserEnt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGWEB.Entities
+{
+    public class UserEnt
+    {
+        public long User_id { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Url_image { get; set; }
+        public long Rol_id { get; set; }
+        public DateTime Register_date { get; set; }
+        public string Rol_name { get; set; }
+        public int Reviews { get; set; }
+        public bool Use_recovery_password { get; set; }
+        public DateTime Date_recovery { get; set; }
+        public string Token { get; set; }
+        public string New_password { get; set; }
+        public string Confirm_new_password { get; set; }
+    }
+}
