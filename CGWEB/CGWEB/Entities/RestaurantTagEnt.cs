f644ada7b9ce5764e3cbdd7d24cc26b45dcfae33
HenryM8 <henryrm8@gmail.com>
2024-06-29 12:38:36 -0600

    Entities for Tag and RestaurantTag

---
+++ CGWEB/CGWEB/Entities/RestaurantTagEnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGWEB.Entities
+{
+    public class RestaurantTagEnt
+    {
+        public long RestaurantTag_Id { get; set; }
+        public long Tag_id { get; set; }
+        public long Restaurant_id { get; set; }
+        public string Restaurant_name { get; set; }
+        public string Tag_name { get; set; }
+    }
+}
---
+++ CGWEB/CGWEB/Entities/TagEnt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGWEB.Entities
+{
+    public class TagEnt
+    {
+        public long Tag_id { get; set; }
+        public string Tag_name { get; set; }
+    }
+}
