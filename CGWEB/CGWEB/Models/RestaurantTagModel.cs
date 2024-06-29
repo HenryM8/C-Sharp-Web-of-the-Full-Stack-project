71e7bf96dab2bfb5c4ff844e016ce959f42a529e
HenryM8 <henryrm8@gmail.com>
2024-06-29 12:39:48 -0600

    Model for Tag and RestaurantTag

---
+++ CGWEB/CGWEB/Models/RestaurantTagModel.cs
@@ -0,0 +1,119 @@
+﻿using CGWEB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace CGWEB.Models
+{
+    public class RestaurantTagModel
+    {
+        string urlWebApi = ConfigurationManager.AppSettings["urlWebApi"].ToString();
+
+        public int AddRestaurantTag(RestaurantTagEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/AddRestaurantTag";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<RestaurantTagEnt> GetRestaurantTags()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetRestaurantTags";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<RestaurantTagEnt>>().Result;
+                }
+
+                return new List<RestaurantTagEnt>();
+            }
+        }
+
+        public RestaurantTagEnt GetRestaurantTag(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetRestaurantTag?q=" + q;
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<RestaurantTagEnt>().Result;
+                }
+
+                return null;
+            }
+        }
+        public List<RestaurantTagEnt> GetRestaurantTagsByRestaurantId(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetRestaurantTagsByRestaurantId?q=" + q;
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<RestaurantTagEnt>>().Result;
+                }
+
+                return new List<RestaurantTagEnt>();
+            }
+        }
+
+        public int ValidateRestaurantTagData(RestaurantTagEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ValidateRestaurantTagData";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ CGWEB/CGWEB/Models/TagModel.cs
@@ -0,0 +1,99 @@
+﻿using CGWEB.Entities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace CGWEB.Models
+{
+    public class TagModel
+    {
+        string urlWebApi = ConfigurationManager.AppSettings["urlWebApi"].ToString();
+
+        public int AddTag(TagEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/AddTag";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PostAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+
+        public List<TagEnt> GetTags()
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetTags";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<List<TagEnt>>().Result;
+                }
+
+                return new List<TagEnt>();
+            }
+        }
+
+        public TagEnt GetTag(long q)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/GetTag?q=" + q;
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                HttpResponseMessage resp = client.GetAsync(url).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<TagEnt>().Result;
+                }
+
+                return null;
+            }
+        }
+
+        public int ValidateTagData(TagEnt entidad)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = urlWebApi + "api/ValidateTagData";
+                string token = HttpContext.Current.Session["Token_session"].ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                JsonContent body = JsonContent.Create(entidad);
+                HttpResponseMessage resp = client.PutAsync(url, body).Result;
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    return resp.Content.ReadFromJsonAsync<int>().Result;
+                }
+
+                return 0;
+            }
+        }
+    }
+}
