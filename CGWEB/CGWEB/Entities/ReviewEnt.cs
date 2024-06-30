13f22c7ea5439f862e41689630c02af14000923c
HenryM8 <henryrm8@gmail.com>
2024-06-30 15:29:19 -0600

    Entity for Reviews

---
+++ CGWEB/CGWEB/Entities/ReviewEnt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CGWEB.Entities
+{
+    public class ReviewEnt
+    {
+        public long Review_id { get; set; }
+        public decimal Rating { get; set; }
+        public string Review_content { get; set; }
+        public DateTime Review_date { get; set; }
+        public bool Is_visible { get; set; }
+        public long Restaurant_id { get; set; }
+        public long User_id { get; set; }
+        public string User { get; set; }
+        public string UserImg { get; set; }
+        public string Restaurant { get; set; }
+    }
+}
