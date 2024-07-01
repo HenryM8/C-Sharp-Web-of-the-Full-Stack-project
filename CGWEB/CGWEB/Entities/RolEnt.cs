ff786874f10d432ea9e94d3c0dac676103856681
HenryM8 <henryrm8@gmail.com>
2024-07-01 13:10:19 -0600

    Entity for roles

---
+++ CGWEB/CGWEB/Entities/RolEnt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CGWEB.Entities
+{
+    public class RolEnt
+    {
+        public long Rol_id { get; set; }
+        public string Rol_name { get; set; }
+    }
+}
